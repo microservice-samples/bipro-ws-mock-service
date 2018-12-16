48d9e23035bde0d014832f95bbf046f2751827aa
hbidani <hassan_bidani@hotmail.de>
2018-12-16 20:30:59 +0100

    added client config to enable the eureka discovery registration

--- src/main/java/de/royal/bipro/BiproWsMockServiceApplication.java
+++ src/main/java/de/royal/bipro/BiproWsMockServiceApplication.java
@@ -2,8 +2,10 @@ package de.royal.bipro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BiproWsMockServiceApplication {
 
 	public static void main(String[] args) {
