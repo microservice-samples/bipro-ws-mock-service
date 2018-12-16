4f1585cdbe28928920cb9162b1d1d810e2dde8cf
hbidani <hassan_bidani@hotmail.de>
2018-12-16 21:04:09 +0100

    updated testcase

--- src/test/java/de/royal/bipro/BiproWsMockServiceApplicationTests.java
+++ src/test/java/de/royal/bipro/BiproWsMockServiceApplicationTests.java
@@ -1,17 +1,10 @@
 package de.royal.bipro;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BiproWsMockServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
 }
 
